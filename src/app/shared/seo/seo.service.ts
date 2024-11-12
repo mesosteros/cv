dd731cdfd75fb4aff172a545175a89db47bf264c
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2024-11-12 23:07:25 +0000

    Add SEO meta tag service

--- src/app/shared/seo/seo.service.ts
+++ src/app/shared/seo/seo.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SeoService {
+  constructor(private meta: Meta, private titleService: Title) {}
 
-  constructor() { }
+  updateMetaTags(
+    pageTitle: string,
+    pageDescription: string,
+    pageImagePath: string
+  ) {
+    this.titleService.setTitle(`Carlos Ermida Santos - ${pageTitle}`);
+
+    // Standard Meta Tags
+    this.meta.addTag({ name: 'description', content: pageDescription });
+
+    this.meta.addTag({
+      name: 'keywords',
+      content:
+        'Front-End Developer, Angular Developer, Angular Expert, Technical Manager, Web Developer, JavaScript Developer, TypeScript Developer, Web Application Development, Team Leadership, Project Management, Experienced Angular Front-End Developer, Front-End Development Consultant',
+    });
+
+    // Open Graph Meta Tags
+
+    this.meta.addTag({
+      property: 'og:title',
+      content: `Carlos Ermida Santos - ${pageTitle}`,
+    });
+
+    this.meta.addTag({
+      property: 'og:description',
+      content: pageDescription,
+    });
+
+    this.meta.addTag({
+      property: 'og:image',
+      content: pageImagePath,
+    });
+  }
 }

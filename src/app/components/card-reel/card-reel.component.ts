4ee4e0a6e665b69f4bb1d9a4667bf48fc753e941
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2024-11-22 00:45:16 +0000

    Card component with lifecycle hooks

--- src/app/components/card-reel/card-reel.component.ts
+++ src/app/components/card-reel/card-reel.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
+@AutoUnsubscribe()
 @Component({
   selector: 'app-card-reel',
   standalone: true,
@@ -9,10 +12,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './card-reel.component.html',
   styleUrl: './card-reel.component.scss',
 })
-export class CardReelComponent {
+export class CardReelComponent implements OnInit, OnDestroy {
   cardItems = [
     { id: 1, title: 'test 1', description: 'testing' },
     { id: 2, title: 'test 2', description: 'testing' },
     { id: 3, title: 'test 3', description: 'testing' },
   ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
 }

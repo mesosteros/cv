7242c888de5417f2d1a794ac41c0bbd8a6b03ed1
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2025-03-04 19:26:07 +0000

    Add loading spinner

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,24 +1,15 @@
-import {
-  Component,
-  Inject,
-  OnInit,
-  PLATFORM_ID,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ContentfulService } from '../../shared/contentful/contentful.service';
-import {
-  CommonModule,
-  isPlatformBrowser,
-  isPlatformServer,
-} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { ResponsiveService } from '../../shared/responsive/responsive.service';
 import { Observable } from 'rxjs';
+import { LoadingService } from '../../shared/loading/loading.service';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-intro',
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingSpinnerComponent],
   providers: [ContentfulService],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
@@ -26,6 +17,7 @@ import { Observable } from 'rxjs';
 export class IntroComponent implements OnInit {
   isMobile$!: Observable<boolean>;
   isDesktop$!: Observable<boolean>;
+  public isLoading: boolean = true;
 
   public defaultIntro: string =
     'With 10+ years of hands-on experience in web projects, I bridge technical excellence and leadership as a Front-End Tech Manager. My expertise centers on modern JavaScript frameworks, particularly Angular, where I’ve architected scalable solutions and mentored teams to deliver polished, user-centric products. As a Tech Manager and Agile Scrum advocate, I thrive in dynamic environments, translating complex requirements into actionable workflows. My leadership extends beyond code: I’ve shaped high-performing teams through technical interviews, on-boarding mentorship, and fostering a culture of continuous growth.';
@@ -35,21 +27,31 @@ export class IntroComponent implements OnInit {
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
     private contentfulService: ContentfulService,
-    private responsiveService: ResponsiveService
+    private responsiveService: ResponsiveService,
+    private loadingService: LoadingService
   ) {}
 
   ngOnInit() {
     this.isMobile$ = this.responsiveService.isMobile$;
     this.isDesktop$ = this.responsiveService.isDesktop$;
 
-    this.contentfulService.getEntries('introduction').then((data: any) => {
-      if (data.items[0].fields.introductionText) {
-        this.introText = documentToHtmlString(
-          data.items[0].fields.introductionText
-        );
-      } else {
-        this.introText = this.defaultIntro;
-      }
-    });
+    this.loadingService.show();
+
+    this.contentfulService
+      .getEntries('introduction')
+      .then((data: any) => {
+        if (data.items[0].fields.introductionText) {
+          this.introText = documentToHtmlString(
+            data.items[0].fields.introductionText
+          );
+        } else {
+          this.introText = this.defaultIntro;
+        }
+      })
+      .catch((error) => (this.introText = this.defaultIntro))
+      .finally(() => {
+        this.loadingService.hide();
+        this.isLoading = false;
+      });
   }
 }

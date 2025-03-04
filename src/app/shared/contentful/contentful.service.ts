eaf8a0bd53878ccac8dced53fd3ca05f424fd211
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2025-03-04 17:20:47 +0000

    Remove markdown from contentful service

--- src/app/shared/contentful/contentful.service.ts
+++ src/app/shared/contentful/contentful.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import * as contentful from 'contentful';
 import { environment } from '../../../environments/environment';
 import { from, map, Observable } from 'rxjs';
-import { marked } from 'marked';
 
 @Injectable({
   providedIn: 'root',
@@ -23,19 +22,15 @@ export class ContentfulService {
 
   // retrieves content mapped to its data fields
   getContent(contentId: string): Observable<any> {
-    const promise = this.client.getEntry(
+    const promise = this.client.getEntries(
       Object.assign({ content_type: contentId })
     );
+
     return from(promise).pipe(
       map((entry: any) => {
-        console.log(entry);
-        return entry.fields;
+        console.log(entry.items);
+        return entry.items;
       })
     );
-  }
-
-  // convert markdown string to
-  markdownToHtml(md: string) {
-    return marked(md);
   }
 }

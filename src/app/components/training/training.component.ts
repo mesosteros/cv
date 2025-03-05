74394973fe66412462f62115a650ac0fe0cce41a
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2025-03-05 23:38:06 +0000

    Add trainings

--- src/app/components/training/training.component.ts
+++ src/app/components/training/training.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { ContentfulService } from '../../shared/contentful/contentful.service';
+import { LoadingService } from '../../shared/loading/loading.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-training',
-  imports: [],
+  imports: [CommonModule, LoadingSpinnerComponent],
+  providers: [ContentfulService],
   templateUrl: './training.component.html',
-  styleUrl: './training.component.scss'
+  styleUrl: './training.component.scss',
 })
-export class TrainingComponent {
+export class TrainingComponent implements OnInit {
+  public isLoading: boolean = true;
+  public trainingData: any = [];
 
+  constructor(
+    private loadingService: LoadingService,
+    private contentfulService: ContentfulService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadingService.show();
+
+    this.contentfulService
+      .getEntries('training')
+      .then((entries: any) => {
+        this.trainingData = entries.items
+          .map((trainingEntry: any) => trainingEntry.fields)
+          .sort((trainingA: any, trainingB: any) =>
+            trainingA.trainingGroup.localeCompare(trainingB.trainingGroup)
+          );
+      })
+      .catch((error) => (this.trainingData = []))
+      .finally(() => {
+        this.loadingService.hide();
+        this.isLoading = false;
+      });
+  }
 }

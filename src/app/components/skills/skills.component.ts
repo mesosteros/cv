340cf3aafd8667a8437579b497bf140ff05e3d94
Carlos Santos <108001241+carlosersantos@users.noreply.github.com>
2025-03-05 11:18:58 +0000

    Add skill cloud

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,18 +1,29 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { CardReelComponent } from '../card-reel/card-reel.component';
 import { SeoService } from '../../shared/seo/seo.service';
 import { ContentfulService } from '../../shared/contentful/contentful.service';
 import { CommonModule, isPlatformServer } from '@angular/common';
+import {
+  CloudData,
+  CloudOptions,
+  TagCloudComponent,
+} from 'angular-tag-cloud-module';
 
 @Component({
   selector: 'app-skills',
-  imports: [CommonModule, CardReelComponent],
+  imports: [CommonModule, TagCloudComponent],
   providers: [SeoService],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
 export class SkillsComponent implements OnInit {
   public skillsData: any;
+
+  options: CloudOptions = {
+    overflow: false,
+  };
+
+  data: CloudData[] = [];
+
   constructor(
     private seoService: SeoService,
     @Inject(PLATFORM_ID) private platformId: Object,
@@ -30,6 +41,17 @@ export class SkillsComponent implements OnInit {
 
     this.contentfulService.getEntries('skills').then((entries: any) => {
       this.skillsData = entries.items.map((skill: any) => skill.fields);
+      this.data = this.skillsData.map((skill: any) => {
+        const skillCloud = {
+          text: skill.title,
+          weight: skill.weight,
+          link: skill.link ? skill.link : null,
+          tooltip: skill.tooltip ? skill.tooltip : null,
+          external: skill.link ? true : false,
+          color: '#' + Math.floor(Math.random() * 16777215).toString(16),
+        };
+        return skillCloud;
+      });
       console.log(this.skillsData[0].title);
     });
   }

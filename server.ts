22da84ca7deac86998a59a478a95411b85b53a73
Carlos Santos <108001241+carlosersantos@users.noreply.github.com>
2025-03-05 10:11:10 +0000

    Add sitemap

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import bootstrap from './src/main.server';
+import xmlbuilder from 'xmlbuilder';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -13,6 +14,32 @@ export function app(): express.Express {
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
   const commonEngine = new CommonEngine();
+
+  const routes = [
+    '/',
+    '/skills',
+    '/professional',
+    '/academic',
+    '/training',
+    '/hobbies',
+  ];
+
+  server.get('/sitemap.xml', (req, res) => {
+    const root = xmlbuilder.create('urlset', {
+      version: '1.0',
+      encoding: 'UTF-8',
+    });
+    root.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+    routes.forEach((route) => {
+      const url = root.ele('url');
+      url.ele('loc', `https://carlosesantos.com${route}`);
+      // You can add more elements like <changefreq> and <priority> here if needed
+    });
+
+    res.header('Content-Type', 'application/xml');
+    res.send(root.end({ pretty: true }));
+  });
 
   server.use((req, res, next) => {
     res.setHeader('X-Robots-Tag', 'all');

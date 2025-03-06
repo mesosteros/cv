fa5520e7072725750cc5bc8572017c547913e489
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2025-03-06 00:59:30 +0000

    Add hobbies

--- src/app/components/hobbies/hobbies.component.ts
+++ src/app/components/hobbies/hobbies.component.ts
@@ -1,11 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from '../../shared/contentful/contentful.service';
+import { LoadingService } from '../../shared/loading/loading.service';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDAndD } from '@fortawesome/free-brands-svg-icons';
+import {
+  faGamepad,
+  faRecordVinyl,
+  faCameraRetro,
+  faFilm,
+  faBook,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-hobbies',
-  imports: [],
+  imports: [CommonModule, LoadingSpinnerComponent, FontAwesomeModule],
+  providers: [ContentfulService, LoadingService],
   templateUrl: './hobbies.component.html',
-  styleUrl: './hobbies.component.scss'
+  styleUrl: './hobbies.component.scss',
 })
-export class HobbiesComponent {
+export class HobbiesComponent implements OnInit {
+  public isLoading: boolean = true;
+  public faDAndD = faDAndD;
+  public faGamepad = faGamepad;
+  public faRecordVinyl = faRecordVinyl;
+  public faCameraRetro = faCameraRetro;
+  public faFilm = faFilm;
+  public hobbiesData: any = [];
 
+  constructor(
+    private loadingService: LoadingService,
+    private contentfulService: ContentfulService
+  ) {}
+
+  ngOnInit() {
+    this.loadingService.show();
+
+    this.contentfulService
+      .getEntries('hobbies')
+      .then((entries: any) => {
+        this.hobbiesData = entries.items
+          .map((entry: any) => entry.fields)
+          .map((hobby: any) => {
+            const hobbyObj = {
+              hobbyName: hobby.hobbyName,
+              hobbyIcon: this.getMatchingIcon(hobby.hobbyName),
+            };
+            return hobbyObj;
+          });
+        console.log(this.hobbiesData);
+      })
+      .catch((error) => (this.hobbiesData = []))
+      .finally(() => {
+        this.loadingService.hide();
+        this.isLoading = false;
+      });
+  }
+
+  private getMatchingIcon(hobbyName: string) {
+    if (hobbyName === 'Video Games') {
+      return faGamepad;
+    } else if (hobbyName === 'TableTop RPGs') {
+      return faDAndD;
+    } else if (hobbyName === 'Photography') {
+      return faCameraRetro;
+    } else if (hobbyName === 'Music') {
+      return faRecordVinyl;
+    } else if (hobbyName === 'Movies') {
+      return faFilm;
+    } else if (hobbyName === 'Books') {
+      return faBook;
+    } else {
+      return faGamepad;
+    }
+  }
 }

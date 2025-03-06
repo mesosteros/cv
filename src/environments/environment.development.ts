67984292e40bd8575ee4b771fd004e47e2ed0c61
Carlos Santos <108001241+carlosersantos@users.noreply.github.com>
2025-03-06 09:54:51 +0000

    Remove secrets from the repo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,7 @@
+import { secrets } from './secrets';
+
 export const environment = {
   production: false,
-  contentful: {
-    spaceId: '1hnpyxq3n4t6',
-    token: 'AhQae2T7RxerrJhwKa5Q_1W4_RHIhFwGoMGIaB9Vv20',
-  },
+  contentful: secrets.contentful,
   hostUrl: 'https://localhost:4200/',
 };

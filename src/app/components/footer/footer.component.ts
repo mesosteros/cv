c3d4abc801a5bfc78d59dca6fb96cdfdbc6a44f1
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2025-03-06 00:24:05 +0000

    Add footer links

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faLinkedin,
+  faGithub,
+  faTwitch,
+  faBluesky,
+} from '@fortawesome/free-brands-svg-icons';
 @Component({
-    selector: 'app-footer',
-    imports: [],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+  selector: 'app-footer',
+  imports: [FontAwesomeModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  public year = new Date().getFullYear();
+  public faLinkedin = faLinkedin;
+  public faGithub = faGithub;
+  public faTwitch = faTwitch;
+  public faBluesky = faBluesky;
 }

3b250684ed86705c1dd942e5f11e7afe6253cbd6
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2025-03-06 00:09:09 +0000

    Add font awesome icons and links

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -3,15 +3,32 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faLinkedin,
+  faGithub,
+  faTwitch,
+  faBluesky,
+} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-navigation',
-  imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatButtonModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent implements OnChanges {
   @Input() hideSideMenu = true;
   mobileMode = false;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faTwitch = faTwitch;
+  faBluesky = faBluesky;
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['hideSideMenu']) {

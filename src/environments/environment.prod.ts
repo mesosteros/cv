d773db88ad0d43bde1cd29d926fc1126b3f0a35c
Carlos Santos <108001241+carlosersantos@users.noreply.github.com>
2025-03-07 11:22:31 +0000

    Fix SSR

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   production: true,
   contentful: {
     spaceId: '1hnpyxq3n4t6',
+    token: CONTENTFUL_ACCESS_TOKEN,
   },
   hostUrl: 'https://carlosesantos.com/',
 };

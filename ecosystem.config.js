ec29ab9f448249a0111f38dd9bc3421fa614e141
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2025-03-08 16:49:58 +0000

    Fix pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'cv',
-        script: 'dist/cv/server/main.js',
+        script: 'dist/cv/server/server.mjs',
         instances: '1',
         exec_mode: 'cluster',
         env_production: {
